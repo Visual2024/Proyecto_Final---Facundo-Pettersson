88a71ed0fa009056271ebe51395462b1193b19d1
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-05-03 23:05:17 -0300

    Cambios

--- PreEntrega3/src/layout/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <div className='bg-red-500 flex flex-row flex-wrap justify-center p-2 mt-10 ' id='footer'>
-      <h4>@Derechos de Autor Reservados</h4>
-    </div>
-  )
-}
